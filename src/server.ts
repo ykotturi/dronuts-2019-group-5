49a209e6690607040a51a2413a3178b4bc2f5400
Bobbie Chen <bobbiechen75@gmail.com>
2019-02-08 14:47:35 -0500

    Initial commit of create-react-app typescript

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-
-const port = 3000;
-const server = app.listen(port, "0.0.0.0", () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-export default server;
